4df5634cb5346060e4f59d01d654d32804c27880
Pablo <pxreturn2@gmail.com>
2020-07-13 21:44:39 -0400

    backend base

---
+++ config.js
@@ -0,0 +1,11 @@
+const API_VERSION = 'v1';
+const IP_SERVER = 'localhost';
+const PORT_DB = 27017;
+const NAME_DB = 'nombre_db';
+
+module.exports = {
+	API_VERSION,
+	IP_SERVER,
+	PORT_DB,
+	NAME_DB
+};
