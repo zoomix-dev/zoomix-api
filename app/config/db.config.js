813ad5682e4a664b9d0eb992c62bbc5706e09d70
Luis Barrientos <luis.barrientos@takeit.co>
2020-07-16 20:48:37 -0400

    change mysql to staging db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+
+const DEV_ENV = {
 	HOST: 'localhost',
 	USER: 'root',
 	PASSWORD: '',
@@ -11,3 +12,20 @@ module.exports = {
 		idle: 10000
 	}
 };
+
+const STAGING_ENV = {
+	HOST: 'x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	USER: 'b7re3vgibyy0ihai',
+	PASSWORD: 'tam22dat6z14laby',
+	DB: 'rrysdjcwr31pua1p',
+	dialect: 'mysql',
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	}
+}
+
+module.exports = STAGING_ENV;
+
